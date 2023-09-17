e24401dc4d1ec17f4ee4c06880dcb01fd23327e3
shariaprema <shariaprema@gmail.com>
2023-09-17 20:27:55 +0600

    first commit

---
+++ Module-44 Tailwind-css-axios-rechart-awsome-component/price-options/src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    
+      <h1 className='text-3xl bg-rose-600 text-center'>Vite + React</h1>
+
+
+
+     
+      
+    </>
+  )
+}
+
+export default App
